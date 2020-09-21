ea94cc3b4799df16d35eec8d76e956c719a4a474
Ayushman Srivastava <ayushman1024@gmail.com>
2020-09-21 16:32:00 +0530

    Automatic commit

--- sync_script.bat
+++ sync_script.bat
@@ -1,13 +1,14 @@
+CD /D %~dp0
 @echo Started execution: %date% %time% >> tasklog.txt
 
 SET prebranch = git branch
 DEL .git\index.lock
-git checkout master
-git fetch origin
+git checkout master >> tasklog.txt
+git fetch origin >> tasklog.txt
 git merge origin/master >> tasklog.txt
 
-git add .
-git commit -m "Automatic commit"
+git add . >> tasklog.txt
+git commit -m "Automatic commit" >> tasklog.txt
 git push >> tasklog.txt
 
 git checkout %prebranch
