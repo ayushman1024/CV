7cf181d259745375bdb3f26bc3f4af58a21e53c8
Ayushman Srivastava <ayushman1024@gmail.com>
2020-11-03 16:58:06 +0530

    Automatic commit

--- sync_driver.bat
+++ sync_driver.bat
@@ -1,4 +1,4 @@
-xcopy Ayushman resume.pdf B:\GDRIVE\CODE\ayushman1024.github.io\resume\  /Y >> tasklog.txt
+xcopy "Ayushman resume.pdf" B:\GDRIVE\CODE\ayushman1024.github.io\resume\  /Y >> tasklog.txt
 START /MIN /W sync_script.bat
 START /MIN B:\GDRIVE\CODE\ayushman1024.github.io\sync_script.bat
 EXIT
